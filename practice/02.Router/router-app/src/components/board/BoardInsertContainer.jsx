b15c16bd637a1a78e66a04f346a2884352905425
Koomaxx86 <bluejjw86@daum.net>
2023-12-04 09:45:42 +0900

    11.31

--- practice/02.Router/router-app/src/components/board/BoardInsertContainer.jsx
+++ practice/02.Router/router-app/src/components/board/BoardInsertContainer.jsx
@@ -13,7 +13,7 @@ const BoardInsertContainer = () => {
             <h1>게시글 쓰기</h1>
             <Link to="/boards">목록</Link>
         </div>
-    )
+    )  
 }
 
 export default BoardInsertContainer
