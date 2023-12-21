14a534985e0c64b6026386143d0cecb288f74ebb
Koomaxx86 <bluejjw86@daum.net>
2023-12-21 18:25:46 +0900

    과제

--- homework/server/routes/todo.js
+++ homework/server/routes/todo.js
@@ -4,7 +4,7 @@ const Todo = require('../models/todo')            // ✅ todo 모델 import
 const router = express.Router()
 
 // 👩‍💻 게시글 목록
-router.get('/', async (req, res) => {
+router.get('/todos', async (req, res) => {
     console.log('게시글 목록...');
     let todoList = []
     try {
@@ -14,96 +14,99 @@ router.get('/', async (req, res) => {
     }
 
     // console.log(todoList);
-    res.render('todo/list', {todoList} )
+    res.json(todoList)
 })
 
 
 
 // 👩‍💻 게시글 등록
-router.get('/insert', (req, res) => {
-    console.log('게시글 등록 화면...');
-    res.render('todo/insert')
-})
+// router.get('/insert', (req, res) => {
+//     console.log('게시글 등록 화면...');
+//     res.render('todo/insert')
+// })
 
 
 
 // 👩‍💻 게시글 등록
-router.post('/', async (req, res) => {
-    console.log('게시글 등록...');
+router.post('/todos', async (req, res) => {
+    console.log('게시글 등록 : ' + req.body);
     // 구조분해할당
-    const { title, writer, content } = req.body;
-    const newTodo = { title, writer, content };
+    const { name, status } = req.body;
+    const newTodo = { name, status };
 
     let result = 0
     try {
-        result = await Todo.create(newTodo)           // ✅ 데이터 등록
+        result = await Todo.create(newTodo)           // 새로 생성된 no를 응답 받아서 setNo 해줘야함
+
     } catch (error) {
         console.log(error);
+        res.status(500).send('Internal Server Error')
     }
 
     console.log(`등록 result : ${result}`);
-    res.redirect('/todo');
+    res.json(newTodo)
 });
 
 // 👩‍💻 게시글 수정 페이지
-router.get('/update/:id', async (req, res) => {
-    console.log('게시글 수정 화면...');
-    console.log(`id : ${req.params.id}`);
-    let id = req.params.id
-    let todo = await Todo.findByPk(id)
-    res.render('todo/update', { todo, id });
-});
+// router.get('/update/:id', async (req, res) => {
+//     console.log('게시글 수정 화면...');
+//     console.log(`id : ${req.params.id}`);
+//     let id = req.params.id
+//     let todo = await Todo.findByPk(id)
+//     res.render('todo/update', { todo, id });
+// });
 
 // 👩‍💻 게시글 수정
-router.post('/update', async (req, res) => {
-    console.log('게시글 수정...');
-    const { id, title, writer, content } = req.body;
+router.put('/todos', async (req, res) => {
+    console.log('게시글 수정 : ' + req.body);
+    const { no, name, status } = req.body;
 
     let result = 0
     try {
         result = await Todo.update({
-            title: title,
-            writer: writer,
-            content: content,
+            nmae : name,
+            status : status,
             upd_date: Sequelize.literal('now()')
         }, {
-            where: {todo_no: id}
+            where: {no : no}
         })
     } catch (error) {
         console.log(error);
+        res.status(500).send('Internal Server Error')
     }
     console.log(`수정 result : ${result}`);
-    res.redirect(`/todo/${id}`);
+    res.status(200).send('Update Result')
 });
 
 // 👩‍💻 게시글 삭제
-router.post('/delete', async (req, res) => {
+router.delete('/todos/:id', async (req, res) => {
     console.log('게시글 삭제...');
-    const id = req.body.id;
+    const id = req.params.id;
 
     let result = 0
     try {
         result = await Todo.destroy({
-            where: { todo_no : id }
+            where: { no : id }
         })
     } catch (error) {
         console.log(error);
+        res.status(500).send('Internal Server Error')
     }
     console.log(`삭제 result : ${result}`);
 
-    res.redirect('/todo');
+    res.status(200).send('Destroy Result')
 });
   
 
 // 👩‍💻 게시글 읽기
 // 요청 경로에 파라미터 매핑 방법 ➡ '/:파라미터명'
-router.get('/:id', async (req, res) => {
+router.get('/todos/:id', async (req, res) => {
     console.log('게시글 읽기 화면...');
     console.log(`id : ${req.params.id}`);
     let id = req.params.id
     let todo = await Todo.findByPk(id)
     console.log(todo);
-    res.render('todo/read', {todo, id})
+    res.json(todo)
 })
 
 
